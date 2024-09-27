336b7004e8d7ccb3f87532961236df22e84cfab7
Daniel medina <danielmedina7778@gmail.com>
2024-09-26 22:43:58 -0500

    modifying navbar

---
+++ src/services/Navbar.js
@@ -0,0 +1,33 @@
+import Component from "@/icons/Component.astro";
+import CSS from "@/icons/CSS.astro";
+import Git from "@/icons/Git.astro";
+import HTML from "@/icons/HTML.astro";
+import Javascript from "@/icons/Javascript.astro";
+
+export const NavbarLinks = [
+  {
+    title: "html",
+    url: "/html/",
+    icono: HTML,
+  },
+  {
+    title: "css",
+    url: "/css/",
+    icono: CSS,
+  },
+  {
+    title: "javascript",
+    url: "/javascript/",
+    icono: Javascript,
+  },
+  {
+    title: "componentes",
+    url: "/ideacomponentes/",
+    icono: Component,
+  },
+  {
+    title: "git",
+    url: "/git/",
+    icono: Git,
+  },
+];
